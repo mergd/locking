d6e7fa3834fe84433c78f778edb2c726f76d6194
mergd <24722014+mergd@users.noreply.github.com>
2026-02-15 13:43:35 +0900

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,20 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
-import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
-const config = defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+export default defineConfig({
   plugins: [
-    devtools(),
-    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     tanstackStart(),
+    nitro({
+      preset: 'vercel',
+    }),
     viteReact(),
   ],
 })
-
-export default config
